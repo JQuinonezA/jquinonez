91b65862534e653685491eb90f6241e07fa43cc9
Jair <jair.quinonez@epn.edu.ec>
2021-01-18 13:34:09 -0500

    four

--- krugerreto/krugerreto/target/debug/deps/krugerreto.d
+++
@@ -1,7 +0,0 @@
-D:\REPOS\jquinonez\krugerreto\krugerreto\target\debug\deps\krugerreto.exe: src\main.rs src\config.rs src\../templates/index.html
-
-D:\REPOS\jquinonez\krugerreto\krugerreto\target\debug\deps\krugerreto.d: src\main.rs src\config.rs src\../templates/index.html
-
-src\main.rs:
-src\config.rs:
-src\../templates/index.html:
